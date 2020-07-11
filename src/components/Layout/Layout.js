01598be8c9f99c0ef5b8b26216d0eeaeca0c451d
huiting-datature <huiting@datature.io>
2020-07-11 12:20:13 +0800

    Implementation of Toolbar & SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+
 import classes from "./Layout.module.css";
 
-const layout = (props) => (
-  <Fragment>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Fragment>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
 
-export default layout;
+  openSideDrawerHandler = () => {
+    this.setState({
+      showSideDrawer: true,
+    });
+  };
+
+  closeSideDrawerHandler = () => {
+    this.setState({
+      showSideDrawer: false,
+    });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Toolbar showSideDrawer={this.openSideDrawerHandler} />
+        <SideDrawer
+          show={this.state.showSideDrawer}
+          closeSideDrawer={this.closeSideDrawerHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Fragment>
+    );
+  }
+}
+
+export default Layout;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import Backdrop from "../Backdrop/Backdrop";
 import classes from "./Modal.module.css";
 
-const modal = (props) => {
-  return (
-    <Fragment>
-      <Backdrop show={props.show} close={props.close} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0",
-        }}
-      >
-        {props.children}
-      </div>
-    </Fragment>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} close={this.props.close} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Modal;
